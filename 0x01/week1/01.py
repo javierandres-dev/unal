4d8452281e2d9c45889a8e99c42bb513a8aa78c9
1270 <1270@holbertonschool.com>
2021-06-04 22:26:15 -0500

    add exercises

---
+++ 0x01/week1/01.py
@@ -0,0 +1,10 @@
+""" Un granjero tiene cincuenta animales entre conejos y gallinas.  Si la
+cantidad de patas de los animales es ciento cuarenta, ¿Cuántos conejos y
+cuántas gallinas tiene el granjero? """
+
+
+def howMany(animals, legs):
+    legsRabbit = 4
+    legsHen = 2
+
+howMany(150, 140)
---
+++ 0x01/week2/01.py
@@ -0,0 +1,4 @@
+""" Si en la UN están podando árboles y cada rama tiene P hojas, y a cada árbol
+le quitaron K ramas, ¿cuántos árboles se deben podar para obtener T hojas?
+"""
+# Problema mal condicionado
