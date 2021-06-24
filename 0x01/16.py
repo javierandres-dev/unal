82a26e6f2d942bb5ea6e10eacb20710420957b92
1270 <1270@holbertonschool.com>
2021-06-24 18:15:26 -0500

    exercise

--- 0x01/16.py
+++ 0x01/16.py
@@ -1,8 +1,7 @@
 """ Recibir un tablero de triqui y determinar si existe un ganador o no """
 
 
-def checkBoard(matrix):
-    # check row
+def checkRows(matrix):
     for row in matrix:
         winner = True
         first_item = row[0]
@@ -12,9 +11,11 @@ def checkBoard(matrix):
                 winner = False
                 break
         if winner:
-            print('Si, existe un ganador - línea horizontal')
-            return
-    # check column
+            return True
+    return False
+
+
+def checkColumns(matrix):
     for column in range(0, 3):
         winner = True
         for item in range(0, 3):
@@ -24,20 +25,52 @@ def checkBoard(matrix):
                 winner = False
                 break
         if winner:
-            print('Si, existe un ganador - línea vertical')
-            return
-    # check diagonal \
+            return True
+    return False
+
+
+def checkFirstDiagonal(matrix):  # \
     for i in range(0, 3):
         winner = True
-        first_item = matrix[i][i]
-        if first_item != matrix[0][0]:
+        first_item = matrix[0][0]
+        item = matrix[i][i]
+        # print(first_item)
+        # print(item)
+        if first_item != item:
             winner = False
             break
-        if winner:
-            print('Si, existe un ganador - línea diagonal \\')
-            return
-    # check diagonal /
-    if not winner:
+    if winner:
+        return True
+    return False
+
+
+def checkSecondDiagonal(matrix):  # /
+    j = 2
+    for i in range(0, 3):
+        winner = True
+        first_item = matrix[0][2]
+        item = matrix[i][j]
+        # print(first_item)
+        # print(item)
+        if first_item != item:
+            winner = False
+            break
+        j -= 1
+    if winner:
+        return True
+    return False
+
+
+def checkBoard(matrix):
+    if checkRows(matrix):
+        print('Si, existe un ganador - línea horizontal')
+    elif checkColumns(matrix):
+        print('Si, existe un ganador - línea vertical')
+    elif checkFirstDiagonal(matrix):
+        print('Si, existe un ganador - línea diagonal \\')
+    elif checkSecondDiagonal(matrix):
+        print('Si, existe un ganador - línea diagonal /')
+    else:
         print('No existe un ganador')
 
 
@@ -46,3 +79,6 @@ checkBoard([['o', '-', '-'], ['x', 'x', 'x'], ['-', '-', 'o']])
 checkBoard([['-', 'x', '-'], ['-', 'x', '-'], ['-', 'x', '-']])
 checkBoard([['x', '-', '-'], ['-', 'x', '-'], ['-', '-', 'x']])
 checkBoard([['-', '-', 'x'], ['-', 'x', '-'], ['x', '-', '-']])
+checkBoard([['x', 'o', 'x'], ['x', 'o', 'x'], ['o', 'x', 'o']])
+checkBoard([['o', 'x', 'o'], ['o', 'x', 'o'], ['x', 'o', 'x']])
+checkBoard([['o', 'x', 'x'], ['o', 'o', 'x'], ['x', 'o', 'x']])
